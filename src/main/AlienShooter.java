969cf9e828e14746e5785504e6d119b4979e9b51
Marcel Loevenich <marcel.loevenich@online.de>
2014-10-22 23:44:39 +0200

    sprites level1-1
    sprites player and snail
    level1-1
    Tile class

--- src/main/AlienShooter.java
+++ src/main/AlienShooter.java
@@ -1,9 +1,21 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import tileMap.Tile;
 
 public class AlienShooter {
 	public static void main(String [] args){
-		Tile tile = new Tile(30, 30, "a1");
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read((new File("../resources/tilesets/basic.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Tile tile = new Tile(30, 30, "a1",img);
 	}
 }
--- src/tileMap/Tile.java
+++ src/tileMap/Tile.java
@@ -6,20 +6,30 @@ public class Tile {
 	private boolean walkable;
 	private int height;
 	private int width;
-	private BufferedImage image;
+	private BufferedImage tile;
 	
-	public Tile(int height,int width, String element){
-		this.height = height;
-		this.width = width;
+	public Tile(int h,int w, String positionInTileset, BufferedImage tileset){
+		this.height = h;
+		this.width = w;
 		
-		String firstChar = element.substring(0, 1);
-		 switch (firstChar) {
-         	case "a":  	walkable = true;
+		String firstChar = positionInTileset.substring(0, 1);
+		int otherChars = Integer.parseInt(positionInTileset.substring(1, positionInTileset.length()));
+		switch (firstChar) {
+			case "a":  	this.walkable = true;
+						this.tile = tileset.getSubimage(otherChars, height, width, height);
                    		break;
-         	case "b":  	walkable = false;
+         	case "b":  	this.walkable = false;
+         				this.tile = tileset.getSubimage(otherChars, height*2, width, height);
          				break;
-		 }
-		 
-		 
+		}
+		
+	}
+	
+	public BufferedImage getTile(){
+		return tile;
+	}
+	
+	public boolean getWalkable(){
+		return walkable;
 	}
 }
